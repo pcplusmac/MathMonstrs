4b8bb726d57abe8cc41806dcee19b8ce5ee6cddb
pcplusmac <s3670994@student.rmit.edu.au>
2020-10-09 19:54:46 +0800

    Initial Commit

---
+++ MathMonstrs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathMonstrs
+//
+//  Created by steven.H on 9/10/20.
+//  Copyright © 2020 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
