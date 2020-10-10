2df617195d7f7764ab53dd6d4f2b452d65826d32
pcplusmac <s3670994@student.rmit.edu.au>
2020-10-10 10:58:49 +0800

    the elements added to the details view and the outlets for the outlet are linked to the detailViewcontroller.

--- MathMonstrs/DetailViewController.swift
+++ MathMonstrs/DetailViewController.swift
@@ -8,7 +8,28 @@
 
 import UIKit
 
+
 class DetailViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var weaponImageView: UIImageView!
+    
+    var monster: Monster? {
+        didSet {
+            refreshUI()
+        }
+    }
+    
+    private func refreshUI() {
+        loadViewIfNeeded()
+        nameLabel.text = monster?.name
+        descriptionLabel.text = monster?.description
+        iconImageView.image = monster?.icon
+        weaponImageView.image = monster?.weapon.image
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
