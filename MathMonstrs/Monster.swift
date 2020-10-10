678093ff48736f2713794d05016fb0fc4d7dfcda
pcplusmac <s3670994@student.rmit.edu.au>
2020-10-10 09:45:17 +0800

    model monster.swift is implemented with the body codes

--- MathMonstrs/Monster.swift
+++ MathMonstrs/Monster.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import UIKit
+
+enum Weapon {
+    case blowgun, ninjaStar, fire, sword, smoke
+    
+    var image: UIImage {
+        switch self {
+        case .blowgun:
+            return UIImage(named: "blowgun.png")!
+        case .fire:
+            return UIImage(named: "fire.png")!
+        case .ninjaStar:
+            return UIImage(named: "ninjastar.png")!
+        case .smoke:
+            return UIImage(named: "smoke.png")!
+        case .sword:
+            return UIImage(named: "sword.png")!
+        }
+    }
+}
+
+class Monster {
+    let name: String
+    let description: String
+    let iconName: String
+    let weapon: Weapon
+    
+    init(name: String, description: String, iconName: String, weapon: Weapon) {
+        self.name = name
+        self.description = description
+        self.iconName = iconName
+        self.weapon = weapon
+    }
+    
+    var icon: UIImage? {
+        return UIImage(named: iconName)
+    }
+}
